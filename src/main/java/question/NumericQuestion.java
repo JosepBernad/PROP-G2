cfaccbc76cb1bf89dabd68f0d263b490d2b1cf0e
Unknown <sergio.paredes25@gmail.com>
2017-04-06 14:11:58 +0200

    Add numeric questions

---
+++ src/main/java/question/NumericQuestion.java
@@ -0,0 +1,24 @@
+package question;
+
+public class NumericQuestion extends Question {
+
+    private Integer min;
+    private Integer max;
+    public Foo hola;
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public void setMin(Integer min) {
+        this.min = min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public void setMax(Integer max) {
+        this.max = max;
+    }
+}
