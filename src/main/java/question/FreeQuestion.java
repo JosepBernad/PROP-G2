39f0a95c540a86f262e3705b08a93de2c5b812e5
Alex Contreras <aleex.28.05@gmail.com>
2017-04-06 14:11:13 +0200

    Add FreeQuestion Class

---
+++ src/main/java/question/FreeQuestion.java
@@ -0,0 +1,14 @@
+package question;
+
+public class FreeQuestion extends Question {
+
+    private  Integer maxSize;
+
+    public Integer getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(Integer maxSize) {
+        this.maxSize = maxSize;
+    }
+}
