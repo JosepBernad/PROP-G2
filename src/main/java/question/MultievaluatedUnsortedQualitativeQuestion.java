c02f3d54733d6436193ae4fee9a7cb3f4c61940e
Josep Bernad Espinosa <jbernadsepinosa@gmail.com>
2017-04-06 14:11:11 +0200

    Added question classes

---
+++ src/main/java/question/MultievaluatedUnsortedQualitativeQuestion.java
@@ -0,0 +1,9 @@
+package question;
+
+/**
+ * Created by SigmundFreud on 06/04/2017.
+ */
+public class MultievaluatedUnsortedQualitativeQuestion extends UnsortedQualitativeQuestion
+{
+    private Integer nMaxAnswers;
+}
