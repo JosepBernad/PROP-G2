fbeab10660cc462d2cb5d30b853fa32fc9aa77c4
Alex Contreras <aleex.28.05@gmail.com>
2017-04-12 15:56:41 +0200

    Add main stupid function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import survey.Survey;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+
+        System.out.println("Welcome to ENQUESTATOR 3000"); // Display the string.
+        Survey survey = new Survey();
+        survey.setId(Integer.valueOf(args[0]));
+        survey.setName(args[1]);
+        survey.save();
     }
 }
