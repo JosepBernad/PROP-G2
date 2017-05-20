98c31526b1d9e8299225260b306cef17a2b76cda
Alex Contreras <aleex.28.05@gmail.com>
2017-05-20 12:44:54 +0200

    Add create user view and controller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class Main extends Application {
 
     public static final String APP_TITLE = "Enquestator 1.0";
@@ -19,7 +20,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
 
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("views/LogIn.fxml"));
+        loader.setLocation(getClass().getResource("views/SignUpView.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root, 400, 350);
@@ -37,4 +38,5 @@ public class Main extends Application {
     public void logInButtonPressed(ActionEvent actionEvent) {
         System.out.println("LogIn Pressed");
     }
+
 }
---
+++ src/main/java/user/SignUpController.java
@@ -0,0 +1,37 @@
+package user;
+
+import com.jfoenix.controls.JFXTextField;
+import exceptions.DuplicatedUsernameException;
+import exceptions.EmptyRequiredAttributeException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
+public class SignUpController {
+
+    @FXML
+    private JFXTextField usernameField;
+
+    @FXML
+    private  JFXTextField nameField;
+
+    @FXML
+    private Label textInformation;
+
+    public void createUserButtonPressed(ActionEvent actionEvent) throws IOException, EmptyRequiredAttributeException {
+        User user = new User();
+        user.setUsername(usernameField.getText());
+        user.setName(nameField.getText());
+        try {
+            user.save();
+            textInformation.setText("User created successfully");
+        } catch (DuplicatedUsernameException e) {
+            textInformation.setText("This user is already taken");
+        }
+        catch (EmptyRequiredAttributeException e) {
+            textInformation.setText("Missing field");
+        }
+    }
+}
