ea0a495c32f990ea853064f9f8410dc70b0f8b3b
Sergio <sergio.paredes25@gmail.com>
2017-06-01 19:44:44 +0200

    Working on sorted questions

--- src/main/java/question/SortedQualitativeQuestionBuilder.java
+++ src/main/java/question/SortedQualitativeQuestionBuilder.java
@@ -1,21 +1,33 @@
 package question;
 
 import exceptions.EmptyRequiredAttributeException;
+import exceptions.NotInRangeException;
+import javafx.scene.control.TextField;
 
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class SortedQualitativeQuestionBuilder extends QuestionBuilder {
 
-    private Set<Option> options;
+    private Map<TextField, TextField> options;
 
-    public SortedQualitativeQuestion build() throws EmptyRequiredAttributeException {
+    public SortedQualitativeQuestion build() throws EmptyRequiredAttributeException, NotInRangeException {
         isEmpty(getStatement());
-        SortedQualitativeQuestion sortedQualitativeQuestion = new SortedQualitativeQuestion();
-        sortedQualitativeQuestion.setOptions(options);
-        return sortedQualitativeQuestion;
+        SortedQualitativeQuestion question = new SortedQualitativeQuestion();
+        question.setStatement(getStatement().getText());
+        Set<Option> optionsSet = new HashSet<>();
+        for (TextField textField : options.keySet())
+            try {
+                optionsSet.add(new Option(options.get(textField).getText(), Integer.parseInt(textField.getText())));
+            } catch (NumberFormatException e) {
+                throw new NotInRangeException();
+            }
+        question.setOptions(optionsSet);
+        return question;
     }
 
-    public SortedQualitativeQuestionBuilder setOptions(Set<Option> options) {
+    public SortedQualitativeQuestionBuilder setOptions(Map<TextField, TextField> options) {
         this.options = options;
         return this;
     }
